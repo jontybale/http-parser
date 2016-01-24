3e7cd6ab89177b73edbb7b1aea0527c6d4ee9c55
jontyb <jontyb@studioskylab.com>
2016-01-24 10:58:00 +0000

    Adding execute tests to confirm that we get kick back when forgetting to add a URI.

---
+++ tests/Command/FetchUriMetaCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jontyb
+ * Date: 24/01/16
+ * Time: 10:50
+ */
+
+namespace JontyBale\HttpParser\Tests\Command;
+
+use JontyBale\HttpParser\Command\FetchUriMetaCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class FetchUriMetaCommandTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Test command execution with no URI, should throw.
+     *
+     * @expectedException        \Symfony\Component\Console\Exception\RuntimeException
+     * @expectedExceptionMessage Not enough arguments (missing: "uri").
+     */
+    public function testExecuteWithNoUri()
+    {
+        $application = new Application();
+        $application->add(new FetchUriMetaCommand());
+
+        $command = $application->find('http-parser:fetch:urimeta');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
+    }
+
+}
