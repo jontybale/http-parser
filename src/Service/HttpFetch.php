7957c353f8e8d8f801a62046643442c8100ed62f
jontyb <jontyb@studioskylab.com>
2016-01-24 11:46:54 +0000

    Adding interface impelementation for HttpFetch service.

--- src/Service/HttpFetch.php
+++ src/Service/HttpFetch.php
@@ -8,6 +8,9 @@
 
 namespace JontyBale\HttpParser\Service;
 
+use JontyBale\HttpParser\Model\Product;
+use JontyBale\HttpParser\Model\UriMeta;
+
 /**
  * Class HttpFetch used as a facade on top of GuzzleHttp to fetch remote data and create
  * our domain models.
@@ -15,7 +18,27 @@ namespace JontyBale\HttpParser\Service;
  * @author jontyb
  * @package JontyBale\HttpParser
  */
-class HttpFetch
+class HttpFetch implements HttpFetchInterface
 {
+    /**
+     * Method to get an array of products from a remote URI.
+     *
+     * @param $uri string
+     * @return Product[]
+     */
+    public function getProducts($uri)
+    {
+        // TODO: Implement getProducts() method.
+    }
 
+    /**
+     * Method to get a UriMeta object for a specific URI.
+     *
+     * @param $uri string
+     * @return UriMeta
+     */
+    public function getUriMeta($uri)
+    {
+        // TODO: Implement getUriMeta() method.
+    }
 }
